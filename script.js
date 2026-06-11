b83c24ed51d18613a74d1ece0c586bdf0c65df5e
EZFelin <EFOliveira4@ucs.br>
2026-06-11 18:22:41 -0300

    Melhorado a responsividade

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const botao = document.querySelector('#todo-button');
 botao.addEventListener("click", (event) => {
     event.preventDefault();
+
     const input = document.querySelector('#todo-input');
+
     console.log(input.value);
+
     if (input.value === '') {
         alert('Por favor, digite uma tarefa.');
         return;
     }
+
     const li = document.createElement('li');
+
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
+
     checkbox.addEventListener('change', () => {
         if (checkbox.checked) {
             li.style.textDecoration = "line-through";
@@ -19,19 +25,45 @@ botao.addEventListener("click", (event) => {
             li.style.opacity = '1';
         }
     });
+
     const span = document.createElement('span');
     span.textContent = input.value;
+
     li.appendChild(checkbox);
     li.appendChild(span);
+
     document.querySelector('#todo-list').appendChild(li);
+
+    checkEmptyList();
+
     input.value = '';
+
     li.classList.add('todo-item');
+
     const botaoExcluir = document.createElement('button');
+
     botaoExcluir.textContent = 'Excluir';
+
     botaoExcluir.addEventListener('click', () => {
         li.remove();
+        checkEmptyList();
     });
+
     botaoExcluir.classList.add('btn-excluir');
     botaoExcluir.innerHTML = '<i class="fa-solid fa-trash"></i>';
+
     li.appendChild(botaoExcluir);
 });
+
+const todoList = document.querySelector('#todo-list');
+const emptyListMessage = document.querySelector('.empty-list');
+
+function checkEmptyList() {
+    if (todoList.children.length === 0) {
+        emptyListMessage.style.display = 'block';
+    } else {
+        emptyListMessage.style.display = 'none';
+    }
+}
+
+checkEmptyList();
